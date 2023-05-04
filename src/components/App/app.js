05b4461b230cda10f30affdcbe825c5e395175d5
carambola <celestebaltazar@yahoo.com>
2023-05-04 16:51:54 -0700

    added app.js and set up files

---
+++ src/components/App/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './app.css';
+import SearchBar from '../SearchBar/searchBar';
+import SearchResults from '../SearchResults/searchResults';
+import Playlists from '../Playlist/playlist';
+import Spotify from '../../utils/spotify';
+
+function App () {
+
+}
+
+
+export default App;
